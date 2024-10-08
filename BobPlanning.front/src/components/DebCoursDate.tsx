42119388ba05d356ab2e009d464c8f24b6f09cee
Khalil ZANZOURI <khalil.zanzouri@student.junia.com>
2024-10-08 09:57:50 +0200

    css tab

--- BobPlanning.front/src/components/DebCoursDate.tsx
+++ BobPlanning.front/src/components/DebCoursDate.tsx
@@ -1,24 +1,21 @@
 import React, { useState } from "react";
-
+import './DebCoursDate.css'; 
 
 const DebCoursDate: React.FC = () => {
   const [startDate, setStartDate] = useState<string>('');
 
-
   return (
-    
-        <div className="date-debut-cours-container">
-          <label htmlFor="debut-date" className="label">Date de début des cours :</label>
-          <input
-            type="date"
-            id="start-date"
-            name="start-date"
-            value={startDate}
-            onChange={(e) => setStartDate(e.target.value)}
-            className="input"
-          />
-        </div>
-     
+    <div className="date-debut-cours-container">
+      <label htmlFor="start-date" className="label">Date de début des cours</label>
+      <input
+        type="date"
+        id="start-date"
+        name="start-date"
+        value={startDate}
+        onChange={(e) => setStartDate(e.target.value)}
+        className="input"
+      />
+    </div>
   );
 };
 
