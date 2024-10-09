65ae8cb6c6d34f402ebfbf342695e411ec493a26
Yoyo338 <146346747+Yoyo338@users.noreply.github.com>
2024-10-09 10:09:09 +0200

    Modification component PeriodesAP

--- BobPlanning.front/src/components/PeriodesAP.tsx
+++ BobPlanning.front/src/components/PeriodesAP.tsx
@@ -1,5 +1,5 @@
 import  { useState } from 'react';
-import { Grid, Box, Select, MenuItem, InputLabel, FormControl, SelectChangeEvent, TextField } from '@mui/material';
+import { Box, Select, MenuItem, InputLabel, FormControl, SelectChangeEvent, TextField, Divider } from '@mui/material';
 import './PeriodesAP.css';
 
 interface PeriodesAPProps {
@@ -31,57 +31,61 @@ export default function PeriodesAP({ nbPeriodesDefaultValue = 5 }: PeriodesAPPro
     };
 
     return (
+        <>  
         <Box className='periodes-ap'>
             <h2>Périodes</h2>
             {/* Select for the number of periods */}
             <FormControl fullWidth margin="normal">
                 <InputLabel id="num-periods-label">Nombre de périodes</InputLabel>
                 <Select
-                    labelId="num-periods-label"
-                    value={numPeriods}
-                    onChange={handleNumPeriodsChange}
-                >
-                    {[...Array(6).keys()].map(i => (
+          labelId="num-periods-label"
+          value={numPeriods}
+          label="Nombre de périodes"
+          onChange={handleNumPeriodsChange}
+        >
+           {[...Array(6).keys()].map(i => (
                         <MenuItem key={i+1} value={i+1}>{i+1}</MenuItem>
                     ))}
-                </Select>
+        </Select>
             </FormControl>
 
+            <Divider className='divider' />
+
             {/* Date and weeks per period */}
-            <Grid container spacing={2}>
                 {dates.map((date, index) => (
-                    <Grid container spacing={2} key={index}>
-                        <Grid item xs={12} sm={6}>
-                            <FormControl fullWidth margin="normal">
-                                <InputLabel shrink>Date de début période {index + 1}</InputLabel>
-                                <TextField
-                                    type="date"
-                                    value={date}
-                                    onChange={(e) => handleDateChange(index, e.target.value)}
-                                    InputLabelProps={{ shrink: true }}
-                                    fullWidth
-                                />
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <FormControl fullWidth margin="normal">
-                                <InputLabel id={`weeks-label-${index}`}>
-                                    Nombre de semaines période {index + 1}
-                                </InputLabel>
-                                <Select
-                                    labelId={`weeks-label-${index}`}
-                                    value={weeks[index]}
-                                    onChange={(e) => handleWeeksChange(index, e.target.value as number)}
-                                >
-                                    {[3, 4, 5, 6].map(week => (
-                                        <MenuItem key={week} value={week}>{week}</MenuItem>
-                                    ))}
-                                </Select>
-                            </FormControl>
-                        </Grid>
-                    </Grid>
+                    <>
+                    <div className='periode'>
+                        <FormControl fullWidth margin="normal">
+                            <InputLabel shrink>Date de début période {index + 1}</InputLabel>
+                            <TextField
+                                type="date"
+                                value={date}
+                                onChange={(e) => handleDateChange(index, e.target.value)}
+                                fullWidth
+                            />
+                        </FormControl>
+                
+                        <FormControl fullWidth margin="normal">
+                            <InputLabel id={`weeks-label-${index}`}>
+                                Nombre de semaines période {index + 1}
+                            </InputLabel>
+                            <Select
+                                labelId={`weeks-label-${index}`}
+                                value={weeks[index]}
+                                label={`Nombre de semaines période ${index + 1}`}
+                                onChange={(e) => handleWeeksChange(index, e.target.value as number)}
+                                fullWidth
+                            >
+                                {[3, 4, 5, 6].map(week => (
+                                    <MenuItem key={week} value={week}>{week}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </div>
+                </>
+                
                 ))}
-            </Grid>
         </Box>
+        </>
     );
 }
--- BobPlanning.front/src/pages/Home.tsx
+++ BobPlanning.front/src/pages/Home.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import TopBar from '../components/TopBar';
 import DebFinCalendrier from '../components/DebFinCalendrier';
 import FullWidthTabs from '../components/TabsPromos';
-import PeriodesAP from '../components/PeriodesAP';
-import ADI1 from '../components/ADI1';
 
 const Home: React.FC = () => {
     return (
@@ -11,12 +9,6 @@ const Home: React.FC = () => {
             <TopBar />
             <DebFinCalendrier />
             <FullWidthTabs />
-        
-            <PeriodesAP />
-            <ADI1 />
-            <PeriodesAP nbPeriodesDefaultValue={2} />
-
-
         </div>
     );
 };
