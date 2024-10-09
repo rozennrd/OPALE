a46c22a91eadeef43e5f3a6b39f452bfd8855c6d
Khalil ZANZOURI <khalil.zanzouri@student.junia.com>
2024-10-09 14:56:57 +0200

    Erreur de saisie : css

--- BobPlanning.front/src/components/DebFinCalendrier.tsx
+++ BobPlanning.front/src/components/DebFinCalendrier.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import './DebFinCalendrier.css';
+import ErrorIcon from '@mui/icons-material/Error';
+
 
 const DebFinCalendrier: React.FC = () => {
   const [startDate, setStartDate] = useState<string>('');
@@ -10,10 +12,14 @@ const DebFinCalendrier: React.FC = () => {
     const newEndDate = e.target.value;
 
     if (startDate && new Date(newEndDate) < new Date(startDate)) {
-      setErrorMessage('La date de fin doit être postérieure à la date de début');
+      const message = 'La date de fin doit être postérieure à la date de début';
+      setErrorMessage(message);
+
     } else if (startDate == '') {
-      setErrorMessage('Veuillez d\'abord renseigner la date de début');
-     } else {
+      const message = 'Veuillez d\'abord renseigner la date de début';
+      setErrorMessage(message);
+
+    } else {
 
       setEndDate(newEndDate);
       setErrorMessage('');
@@ -23,39 +29,43 @@ const DebFinCalendrier: React.FC = () => {
 
 
 
-return (
-  <div className="deb-fin-calendrier">
+  return (
+    <div className="deb-fin-calendrier">
 
-    <div className="saisie-date">
-      <div className="saisie-date-title">
-        <h2>Calendrier {getYear(startDate)}-{getYear(endDate)}</h2>
+      <div className="saisie-date">
+        <div className="saisie-date-title">
+          <h2>Calendrier {getYear(startDate)}-{getYear(endDate)}</h2>
+        </div>
+        <div className="date-input-container">
+          <label htmlFor="start-date" className="label">Date de début </label>
+          <input
+            type="date"
+            id="start-date"
+            name="start-date"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+            className="input"
+          />
+        </div>
+        <div className="date-input-container">
+          <label htmlFor="end-date" className="label">Date de fin </label>
+          <input
+            type="date"
+            id="end-date"
+            name="end-date"
+            value={endDate}
+            onChange={handleEndDateChange}
+            className="input"
+          />
+        </div>
+        {errorMessage && (
+          <div className="error-message">
+            <ErrorIcon  />
+           <span>{errorMessage} </span>
+          </div>)}
       </div>
-      <div className="date-input-container">
-        <label htmlFor="start-date" className="label">Date de début </label>
-        <input
-          type="date"
-          id="start-date"
-          name="start-date"
-          value={startDate}
-          onChange={(e) => setStartDate(e.target.value)}
-          className="input"
-        />
-      </div>
-      <div className="date-input-container">
-        <label htmlFor="end-date" className="label">Date de fin </label>
-        <input
-          type="date"
-          id="end-date"
-          name="end-date"
-          value={endDate}
-          onChange={handleEndDateChange}
-          className="input"
-        />
-      </div>
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
     </div>
-  </div>
-);
+  );
 };
 
 export default DebFinCalendrier;
