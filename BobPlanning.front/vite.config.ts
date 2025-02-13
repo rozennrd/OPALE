617897e54f90530aaf27b2251a087d5e41412438
Yoan <yoan_bernard@yahoo.fr>
2025-02-13 10:48:17 +0100

    Update vite.config.ts

--- BobPlanning.front/vite.config.ts
+++ BobPlanning.front/vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '../.env' }); // Charge le .env à la racine de BobPlanning
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.JPG', '**/*.PNG'], // Inclure les fichiers d'images comme assets
 });
-  
-
