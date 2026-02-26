1200d09a1811092493140153f8e54efd413cd2fd
rozennrd <r_renaud@gmx.fr>
2026-02-26 12:03:11 +0100

    :bug: repaired events

--- Opale.Front/src/components/common/ActionButtonsWithConfirm.tsx
+++ Opale.Front/src/components/common/ActionButtonsWithConfirm.tsx
@@ -61,6 +61,7 @@ export const ActionButtonsWithConfirm: React.FC<ActionButtonsWithConfirmProps> =
     deleteCancelLabel = 'Annuler',
     onBeforeSaveClick,
     onAfterSaveConfirm,
+    disabled,
     onSave,
     onCancel,
 }) => {
